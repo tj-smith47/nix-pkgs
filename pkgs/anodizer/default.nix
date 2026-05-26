700e223701e08089f46d20c0a2568b4e57290ad5
TJ Smith <tj@jarvispro.io>
2026-05-26 04:21:25 +0000

    anodizer 0.5.0

--- pkgs/anodizer/default.nix
+++ pkgs/anodizer/default.nix
@@ -10,21 +10,21 @@
 let
   selectSystem = attrs: attrs.${stdenvNoCC.hostPlatform.system} or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
   urlMap = {
-    aarch64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.4.0/anodizer-0.4.0-darwin-arm64.tar.gz";
-    aarch64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.4.0/anodizer-0.4.0-linux-arm64.tar.gz";
-    x86_64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.4.0/anodizer-0.4.0-darwin-amd64.tar.gz";
-    x86_64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.4.0/anodizer-0.4.0-linux-amd64.tar.gz";
+    aarch64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.5.0/anodizer-0.5.0-darwin-arm64.tar.gz";
+    aarch64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.5.0/anodizer-0.5.0-linux-arm64.tar.gz";
+    x86_64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.5.0/anodizer-0.5.0-darwin-amd64.tar.gz";
+    x86_64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.5.0/anodizer-0.5.0-linux-amd64.tar.gz";
   };
   shaMap = {
-    aarch64-darwin = "";
-    aarch64-linux = "";
-    x86_64-darwin = "";
-    x86_64-linux = "";
+    aarch64-darwin = "1jsg16897vx5vfsb23z4pb432rilzix2p5rzhpxrfc4k3343gy7v";
+    aarch64-linux = "0i0dd83v18ykxz9d2aiksbq4c2dqdbbqfbiydnnxxjf21kcsxyhv";
+    x86_64-darwin = "1za74rn29fy0bsn84k1x2r5yx03m1pn384y42nk67a38rs2xclv8";
+    x86_64-linux = "08zd3851jfv42j4ybn3barad588iap89vg9qv82lh8v088x4fh6d";
   };
 in
 stdenvNoCC.mkDerivation {
   pname = "anodizer";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchurl {
     url = selectSystem urlMap;
