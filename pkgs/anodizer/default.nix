720e7435444f32b0e35e3049dd5412d08e3a5a27
TJ Smith <tj@jarvispro.io>
2026-06-09 03:20:23 +0000

    anodizer 0.7.0

--- pkgs/anodizer/default.nix
+++ pkgs/anodizer/default.nix
@@ -10,21 +10,21 @@
 let
   selectSystem = attrs: attrs.${stdenvNoCC.hostPlatform.system} or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
   urlMap = {
-    aarch64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.6.0/anodizer-0.6.0-darwin-arm64.tar.gz";
-    aarch64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.6.0/anodizer-0.6.0-linux-arm64.tar.gz";
-    x86_64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.6.0/anodizer-0.6.0-darwin-amd64.tar.gz";
-    x86_64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.6.0/anodizer-0.6.0-linux-amd64.tar.gz";
+    aarch64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.7.0/anodizer-0.7.0-darwin-arm64.tar.gz";
+    aarch64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.7.0/anodizer-0.7.0-linux-arm64.tar.gz";
+    x86_64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.7.0/anodizer-0.7.0-darwin-amd64.tar.gz";
+    x86_64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.7.0/anodizer-0.7.0-linux-amd64.tar.gz";
   };
   shaMap = {
-    aarch64-darwin = "1dgsdqd0kf60lh6fkldpmmsgmy1garpkg08aa19sbd4s42jvjjki";
-    aarch64-linux = "14z1yygn0qf0p5mp8z460gc31h6rs4d3d5fg3xaaam7xds8vmcsw";
-    x86_64-darwin = "1zkkn9qgkz1a1jnq47b9zzqld8m9vq0i18f0a2d9mnk62n3vip0q";
-    x86_64-linux = "17216kk447al0zw9a6d4qy32zgxgw52w7im791vr0lnz3cyb6avm";
+    aarch64-darwin = "0gnw632knia3xiyh5v62mhs7y7icx15lqbba08pf9pmmagfp7hhv";
+    aarch64-linux = "0m7c5mvhsp78dp2sff64daadwps7d8hfrvwpgq60nrvc26bwpvn0";
+    x86_64-darwin = "1363hvp6080nsjvxkicjdif5amgaf1jv0xs4yg4ql7fh0909l51c";
+    x86_64-linux = "10syjs8dzay66r1836h3nywlw9d3z4wh6ipqdg7h04bppp98khyy";
   };
 in
 stdenvNoCC.mkDerivation {
   pname = "anodizer";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchurl {
     url = selectSystem urlMap;
