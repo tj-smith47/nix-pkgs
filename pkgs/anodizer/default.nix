40318466feed2969aa9859122a3d34b028a41034
TJ Smith <tj@jarvispro.io>
2026-06-25 02:54:40 +0000

    anodizer 0.12.1

--- pkgs/anodizer/default.nix
+++ pkgs/anodizer/default.nix
@@ -9,21 +9,21 @@
 }: let
   selectSystem = attrs: attrs.${stdenvNoCC.hostPlatform.system} or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
   urlMap = {
-    aarch64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.0/anodizer-0.12.0-darwin-arm64-extra.tar.xz";
-    aarch64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.0/anodizer-0.12.0-linux-arm64-extra.tar.xz";
-    x86_64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.0/anodizer-0.12.0-darwin-amd64-extra.tar.xz";
-    x86_64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.0/anodizer-0.12.0-linux-amd64-extra.tar.xz";
+    aarch64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.1/anodizer-0.12.1-darwin-arm64-extra.tar.xz";
+    aarch64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.1/anodizer-0.12.1-linux-arm64-extra.tar.xz";
+    x86_64-darwin = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.1/anodizer-0.12.1-darwin-amd64-extra.tar.xz";
+    x86_64-linux = "https://github.com/tj-smith47/anodizer/releases/download/v0.12.1/anodizer-0.12.1-linux-amd64-extra.tar.xz";
   };
   shaMap = {
-    aarch64-darwin = "1p3ljgrih0d9pf60619dyjxg249fjgsqn3a7ljkn1wfniyg582r4";
-    aarch64-linux = "03ih7dgxjca85nlp9kfd42wqiwyylizsalc4sd35y8cdciy4db9x";
-    x86_64-darwin = "1ryx88aazn1fhkfhlbzyqky9bn4z0vrn3qj8hc3m3gfmvparyki7";
-    x86_64-linux = "0agfyjxmrwc5g1abj0jmw31kii36cf912s947v8gbdhg5215s9si";
+    aarch64-darwin = "0iisy2a2rmf1mqj5wr24d11lzklgck5513sj8yj108gp2xjzby7r";
+    aarch64-linux = "0242b6w3zg2c28858v5rxj7rf7q9pcq88li6smwrgs64mmyjxdi9";
+    x86_64-darwin = "1di6d05gwa7sf72hwxqcjywv826rc9vqa2v7pkapwg3zm8j9phv9";
+    x86_64-linux = "0wjwdksj6mm4x5kp8hfb8as7cc7aiqvi6x210hyg71qw5jzshhwz";
   };
 in
   stdenvNoCC.mkDerivation {
     pname = "anodizer";
-    version = "0.12.0";
+    version = "0.12.1";
 
     src = fetchurl {
       url = selectSystem urlMap;
